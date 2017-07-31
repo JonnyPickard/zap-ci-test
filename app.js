3a6b9d15c75cd3b5d22e0d2924b67b7f54d95bf8
jonnypickard <jonathandpickard@gmail.com>
2017-07-31 11:01:59 +0100

    Zap & travis

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var PORT = process.env.PORT || 3000;
+var {NODE_ENV} = process.env;
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -25,22 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+const server = app.listen(port, (req, res) => {
+  if (NODE_ENV === 'test') server.close();
+  console.log('Server listening on port: ', port)
 });
 
 module.exports = app;
